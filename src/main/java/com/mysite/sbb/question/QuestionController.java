2d7b6e3d47d261ac26a564ba5cf5ef1f0724cd36
rlalastjd782 <rlalastjd782@naver.com>
2022-10-07 11:01:56 +0900

    주석 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -37,7 +37,11 @@ public class QuestionController {
     public String questionCreate() {
         return "question_form";
     }
-
+    // 널값 검증하기
+    //valid ->  QuestionForm의 @NotEmpty, @Size 등으로 설정한 검증 기능이 동작한다.
+    //BindingResult 매개변수는 @Valid 애너테이션으로 인해 검증이 수행된 결과를 의미하는 객체이다.
+    //*BindingResult 매개변수는 항상 @Valid 매개변수 바로 뒤에 위치해야 한다. 만약 2개의 매개변수의 위치가 정확하지 않다면
+    // @Valid만 적용이 되어 입력값 검증 실패 시 400 오류가 발생한다.
     @PostMapping("/create")
     public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
